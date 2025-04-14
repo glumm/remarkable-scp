b2b64b9b982d8652d2867f1aba36166a612a355a
glumm <jlminot13@gmail.com>
2025-04-13 21:31:55 -0400

    capturing valid ip address

--- scp-remark.sh
+++ scp-remark.sh
@@ -4,17 +4,28 @@
 #zenity --file-selection --tile "File to Transfer" --multiple --file-filter='*.mm *.png *.page *.sh *.txt'
 
 # Response=$(zenity --file-selection --multiple); echo $Response
-validate_ip(){
+
+valid_ip() {
   local ip=$1
-  local IFS=.; 
+  local IFS=.; local -a a=($ip) 
+  #regex to validate pattern
+    [[ $ip =~ ^[0-9]+(\.[0-9]+){3}$ ]] || return 1
+  #validating octets 
+  local quad
+  for quad in {0..3}; do
+    [[ "${a[$quad]}" -gt 255 ]] && return 1
+  done
+  return 0
 }
 
 
 
 ip=$(zenity --entry --text='Please Enter the IP address of your Remarkable Tablet: '); 
 
-if []; then 
+if valid_ip "$ip"; then
+  zenity --info --width 300 --text "IP entered correctly"; 
 else 
+  zenity --error --width 300 --text "IP does not follow standard convention. Please try again"; 
 fi
 echo "you entered $ipaddr";
 
